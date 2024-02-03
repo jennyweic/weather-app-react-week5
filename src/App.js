ec87affe999456eb385f95b952fc8f1b5d35e6e2
jennyweic <weicchan@gmail.com>
2024-02-03 11:11:14 +0000

    Added weather search form feature

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App using React</h1>
+        <h1>Your local weather app</h1>
         <Weather />
         <footer>
           This Weather App coded with React project is created by Jennifer and
--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,22 @@
 import React from "react";
 import "./Weather.css";
 export function Weather() {
-  return <div className="Weather">Hello</div>;
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter your city"
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" />
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 }
