cdf6be971ceb781353452da94d315dc1d4080423
jennyweic <weicchan@gmail.com>
2024-02-03 10:23:18 +0000

    Fix code  add div id root back into index.html, and added new text in app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">TESTING</div>;
+  return (
+    <div className="App">
+      Weather App using React
+      <p>This is Jennifer's Weather App made using React.</p>
+      <a
+        href="https://github.com/jennyweic/weather-app-react-week5"
+        target="_blank"
+      >
+        Github profile.
+      </a>
+    </div>
+  );
 }
