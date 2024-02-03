531534b7665eb02b9a5611e6e82e222cc70f6537
jennyweic <weicchan@gmail.com>
2024-02-03 13:46:03 +0000

    Added api to pull of weather information when searched

--- src/Weather.js
+++ src/Weather.js
@@ -1,25 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
+
 export function Weather() {
+  let [city, setCity] = useState("");
+  let [weatherData, setWeatherData] = useState({
+    temperature: null,
+    description: null,
+    wind: null,
+    humiditiy: null,
+    feels_like: null,
+  });
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  //Do something with the data
+  function getResponse(event) {
+    event.preventDefault();
+
+    let url = `https://api.shecodes.io/weather/v1/current?query=${city}&key=698445003bc2a9cbfcb050ae4t74oc8b`;
+    axios.get(url).then((data) => {
+      const weatherData = {
+        temperature: Math.round(data.data.temperature.current),
+        description: data.data.condition.description,
+        wind: data.data.wind.speed,
+        humidity: data.data.temperature.humidity,
+        feels_like: data.data.temperature.feels_like,
+      };
+
+      setWeatherData(weatherData);
+    });
+  }
+
   return (
     <div className="Weather">
-      <form>
+      <form onSubmit={getResponse}>
         <div className="row">
           <div className="col-9">
             <input
-              className="Search-text-btn"
+              className="Search-text-btn form-control"
               type="search"
               placeholder="Enter your city"
-              className="form-control"
+              onChange={updateCity}
             />
           </div>
           <div className="col-3">
-            <input className="btn" type="submit" value="Search" />
+            <input className="btn" type="Submit" value="Search" />
           </div>
         </div>
       </form>
 
-      <h1>London</h1>
+      <h1>{city}</h1>
       <p>Sat, 11:00 am</p>
 
       <div className="row">
@@ -31,17 +64,16 @@ export function Weather() {
                 src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
                 alt="clear-sky-icon"
               />
-              Clear
+              {weatherData.description}
             </li>
-            <li className="temperature">12°C</li>
+            <li className="temperature">{weatherData.temperature}°C</li>
           </ul>
         </div>
         <div className="col-6">
           <ul className="details">
-            <li>Feels like 10°C</li>
-            <li>Percepitation 2%</li>
-            <li>Wind 5km/h</li>
-            <li>Humidity 15%</li>
+            <li>Feels like {weatherData.feels_like}°C</li>
+            <li>Wind {weatherData.wind}km/h</li>
+            <li>Humidity {weatherData.humidity}%</li>
           </ul>
         </div>
       </div>
