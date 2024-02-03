55e562ffb7c52c1b5e440a80a775b4491b57e1a6
jennyweic <weicchan@gmail.com>
2024-02-03 10:43:21 +0000

    Installed Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App using React</h1>
-      <footer>
-        This Weather App coded with React project is created by Jennifer and is
-        opened-sourced.
-        <a
-          href="https://github.com/jennyweic/weather-app-react-week5"
-          target="_blank"
-        >
-          Github profile.
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App using React</h1>
+        <footer>
+          This Weather App coded with React project is created by Jennifer and
+          is opened-sourced.
+          <a
+            href="https://github.com/jennyweic/weather-app-react-week5"
+            target="_blank"
+          >
+            Github profile.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
