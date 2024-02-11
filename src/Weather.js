6c6da73cb31d0f048854646f217b7959fbc988ed
jennyweic <weicchan@gmail.com>
2024-02-11 14:04:40 +0000

    Inserted conditioning rules when there's no data

--- src/Weather.js
+++ src/Weather.js
@@ -4,13 +4,19 @@ import "./Weather.css";
 
 export function Weather() {
   let [city, setCity] = useState("");
-  let [weatherData, setWeatherData] = useState({
-    temperature: null,
-    description: null,
-    wind: null,
-    humiditiy: null,
-    feels_like: null,
-  });
+
+  /**
+   * When data is ready, it looks like this:
+   * {
+    temperature: 10,
+    description: windy,
+    wind: 10,
+    humiditiy: 10,
+    feels_like: 1-,
+  }
+  We set it intially to false as there is no data...
+   */
+  let [weatherData, setWeatherData] = useState(false);
 
   function updateCity(event) {
     setCity(event.target.value);
@@ -53,31 +59,36 @@ export function Weather() {
         </div>
       </form>
 
-      <h1>{city}</h1>
-      <p>{weatherData.date}</p>
-
-      <div className="row">
-        <div className="col-6">
-          <ul>
-            <li className="condition-description text-capitalize">
-              {" "}
-              <img
-                src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
-                alt="clear-sky-icon"
-              />
-              {weatherData.description}
-            </li>
-            <li className="temperature">{weatherData.temperature}°C</li>
-          </ul>
+      {weatherData ? (
+        <div>
+          <h1>{city}</h1>
+          <p>{weatherData.date}</p>
+          <div className="row">
+            <div className="col-6">
+              <ul>
+                <li className="condition-description text-capitalize">
+                  {" "}
+                  <img
+                    src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
+                    alt="clear-sky-icon"
+                  />
+                  {weatherData.description}
+                </li>
+                <li className="temperature">{weatherData.temperature}°C</li>
+              </ul>
+            </div>
+            <div className="col-6">
+              <ul className="details">
+                <li>Feels like {weatherData.feels_like}°C</li>
+                <li>Wind {weatherData.wind}km/h</li>
+                <li>Humidity {weatherData.humidity}%</li>
+              </ul>
+            </div>
+          </div>
         </div>
-        <div className="col-6">
-          <ul className="details">
-            <li>Feels like {weatherData.feels_like}°C</li>
-            <li>Wind {weatherData.wind}km/h</li>
-            <li>Humidity {weatherData.humidity}%</li>
-          </ul>
-        </div>
-      </div>
+      ) : (
+        <div>make this pretty</div>
+      )}
     </div>
   );
 }
