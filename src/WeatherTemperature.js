40261cfc7142ad14fd334822409306f6437e1250
jennyweic <weicchan@gmail.com>
2024-02-11 19:46:07 +0000

    Created new WeatherTemperate component to toggle between celsius and fahrenheit

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,10 +1,46 @@
-import React from "react";
-import { Weather } from "./Weather";
+import React, { useState } from "react";
 
 export default function WeatherTemperature({ celsius }) {
-  return (
-    <div>
-      <li className="temperature">{celsius}°C</li>
-    </div>
-  );
+  let [unit, setUnit] = useState("celsius");
+
+  function showFahrenheit(event) {
+    event.preventDefault();
+    setUnit("fahrenheit");
+  }
+
+  function showCelsius(event) {
+    event.preventDefault();
+    setUnit("celsius");
+  }
+
+  function fahrenheit() {
+    return (celsius * 9) / 5 + 32;
+  }
+
+  if (unit === "celsius") {
+    return (
+      <div>
+        <span className="temperature">{celsius}</span>
+        <span className="unit">
+          °C | {""}
+          <a href="/" onClick={showFahrenheit}>
+            °F
+          </a>
+        </span>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <span className="temperature">{Math.round(fahrenheit())}</span>
+        <span className="unit">
+          <a href="/" onClick={showCelsius}>
+            °C |
+          </a>
+          {""}
+          °F
+        </span>
+      </div>
+    );
+  }
 }
