6fe5c4f4e101a29a728c181120081ecd177faac7
jennyweic <weicchan@gmail.com>
2024-02-21 11:05:58 +0000

    Created WeatherForecast css file and amended the first asset

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Weather } from "./Weather";
+import "./WeatherForecast.css";
 
 export default function WeatherForecast(props) {
   console.log(props);
@@ -7,8 +8,12 @@ export default function WeatherForecast(props) {
     <div className="WeatherForecast">
       <div className="row">
         <div className="col">
-          Thu icon <img src={props.icon} />
-          19 10
+          <div className="WeatherForecast-day">Thu</div>
+          <img src={props.icon} />
+          <div className="WeatherForecast-temperature">
+            <span className="WeatherForecast-temperature-max"> 19°</span>
+            <span className="WeatherForecast-temperature-min"> 10°</span>
+          </div>
         </div>
       </div>
     </div>
