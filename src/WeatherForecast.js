8c39438f2c749e4fc50e3a66e35313b260a40ecb
jennyweic <weicchan@gmail.com>
2024-02-21 11:23:45 +0000

    Added axios to pull through API data

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,9 +1,21 @@
 import React from "react";
 import { Weather } from "./Weather";
 import "./WeatherForecast.css";
+import axios from "axios";
 
 export default function WeatherForecast(props) {
   console.log(props);
+
+  function handleResponse(response) {
+    console.log(response);
+  }
+
+  let longitude = -0.1275;
+  let latitude = 51.50722;
+  let apiURL = `https://api.shecodes.io/weather/v1/forecast?lon=${longitude}&lat=${latitude}&key=698445003bc2a9cbfcb050ae4t74oc8b`;
+
+  axios.get(apiURL).then(handleResponse);
+
   return (
     <div className="WeatherForecast">
       <div className="row">
