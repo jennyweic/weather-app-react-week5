ce70e6dc1f12e9e519fa35ace89293e7a9d95653
jennyweic <weicchan@gmail.com>
2024-02-25 18:37:48 +0000

    If we have foreacst data is ready to load from outside word use state, condition rendering

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Weather } from "./Weather";
 import "./WeatherForecast.css";
 import axios from "axios";
@@ -6,28 +6,34 @@ import axios from "axios";
 export default function WeatherForecast(props) {
   console.log(props);
 
+  let [forecastLoaded, setForecastLoaded] = useState(false);
+
   function handleResponse(response) {
     console.log(response);
   }
 
-  let longitude = props.coordinates_longitude;
-  let latitude = props.coordinates_latitude;
-  let apiURL = `https://api.shecodes.io/weather/v1/forecast?lon=${longitude}&lat=${latitude}&key=698445003bc2a9cbfcb050ae4t74oc8b`;
+  if (forecastLoaded) {
+    let longitude = props.coordinates_longitude;
+    let latitude = props.coordinates_latitude;
+    let apiURL = `https://api.shecodes.io/weather/v1/forecast?lon=${longitude}&lat=${latitude}&key=698445003bc2a9cbfcb050ae4t74oc8b`;
 
-  axios.get(apiURL).then(handleResponse);
+    axios.get(apiURL).then(handleResponse);
 
-  return (
-    <div className="WeatherForecast">
-      <div className="row">
-        <div className="col">
-          <div className="WeatherForecast-day">Thu</div>
-          <img src={props.icon} />
-          <div className="WeatherForecast-temperature">
-            <span className="WeatherForecast-temperature-max"> 19°</span>
-            <span className="WeatherForecast-temperature-min"> 10°</span>
+    return null;
+  } else {
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          <div className="col">
+            <div className="WeatherForecast-day">Thu</div>
+            <img src={props.icon} />
+            <div className="WeatherForecast-temperature">
+              <span className="WeatherForecast-temperature-max"> 19°</span>
+              <span className="WeatherForecast-temperature-min"> 10°</span>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
