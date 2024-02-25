a921686a822e918a461ba6f5b4718c6b9aa3cc02
jennyweic <weicchan@gmail.com>
2024-02-25 19:09:03 +0000

    Fixed city, lat and lon by adding if statement return null, added dynamic forecast 1 temp

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -11,8 +11,20 @@ export default function WeatherForecast(props) {
 
   function handleResponse(response) {
     console.log(response);
-    setForecastData(response.data);
+
+    const forecastData = {
+      max_temperature: response.data.daily[0].temperature.maximum,
+      min_temperature: response.data.daily[0].temperature.minimum,
+    };
+
+    setForecastData(forecastData);
+    setForecastLoaded(true);
   }
+
+  let longitude = props.coordinates_longitude;
+  let latitude = props.coordinates_latitude;
+
+  if (!longitude || !latitude) return null;
 
   if (forecastLoaded) {
     return (
@@ -22,20 +34,21 @@ export default function WeatherForecast(props) {
             <div className="WeatherForecast-day">Thu</div>
             <img src={props.icon} />
             <div className="WeatherForecast-temperature">
-              <span className="WeatherForecast-temperature-max"> 19°</span>
-              <span className="WeatherForecast-temperature-min"> 10°</span>
+              <span className="WeatherForecast-temperature-max">
+                {Math.round(forecastData.max_temperature)}°
+              </span>
+              <span className="WeatherForecast-temperature-min">
+                {Math.round(forecastData.min_temperature)}°
+              </span>
             </div>
           </div>
         </div>
       </div>
     );
   } else {
-    let longitude = props.coordinates_longitude;
-    let latitude = props.coordinates_latitude;
     let apiURL = `https://api.shecodes.io/weather/v1/forecast?lon=${longitude}&lat=${latitude}&key=698445003bc2a9cbfcb050ae4t74oc8b`;
-
+    console.log(apiURL);
     axios.get(apiURL).then(handleResponse);
-
     return null;
   }
 }
